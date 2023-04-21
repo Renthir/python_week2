6a04e3a6fac39c1792b03d14793d9de6d27f1caa
Seth Reid <reid.seth.96@gmail.com>
2023-04-21 17:26:24 -0600

    completed basic functions

---
+++ controller.py
@@ -0,0 +1,23 @@
+import csv
+
+
+def get_cupcakes(file):
+    cupcake_list = []
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+    
+        for row in reader:
+            cupcake_list.append(row)
+    return cupcake_list
+
+def find_cupcakes(file, name):
+    for cupcake in get_cupcakes(file):
+        if cupcake["name"] == name:
+            return cupcake
+    return None
+
+def add_cupcake_dict(file, cupcake):
+    with open(file, 'a', newline='\n') as csvfile:
+        fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(cupcake)
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
+import csv
+from controller import get_cupcakes, find_cupcakes, add_cupcake_dict
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -8,15 +11,27 @@ def index():
 
 @app.route('/cupcakes')
 def all_cupcakes():
-    return render_template('cupcakes.html')
+    cupcake_list = get_cupcakes("cupcakes.csv")
+    return render_template('cupcakes.html', cupcakes=cupcake_list)
 
-@app.route('/indiv_cupcake')
+@app.route('/individual-cupcake/<cupcake>')
 def indiv_cupcake():
     return render_template('indiv_cupcake.html')
 
 @app.route('/order')
 def order():
     return render_template('order.html')
+
+@app.route('/add-cupcake/<name>')
+def add_cupcake(name):
+    cupcake = find_cupcakes("cupcakes.csv", name)
+    if cupcake:
+        add_cupcake_dict("orders.csv", cupcake)
+        return redirect(url_for("index"))
+    else:
+        return "Sorry, cupcake not found."
+
+
 
 
 if __name__  == "__main__":
